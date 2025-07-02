37bb4aba98fd0d4ffbdd93f22190ed175d4c694f
saad <ssaadn.555@gmail.com>
2025-07-03 02:57:03 +0600

    gemini API added

--- llm_handler.js
+++ llm_handler.js
@@ -1,54 +1,66 @@
 let hfToken = null;
+let apiKey = null;
 
+// https://router.huggingface.co/fireworks-ai/inference/v1/chat/completions
+// headers: {
+//         "Authorization": `Bearer ${TOKEN}`,
+//         "Content-Type": "application/json"
+//       }
 
-// https://router.huggingface.co/novita/v3/openai/chat/completions
-
-// {
-//         model: "deepseek/deepseek-r1-0528",
-//         stream: false,
+// body: JSON.stringify({
 //         messages: [
 //           {
 //             role: "user",
 //             content: message.payload || "What is the capital of France?"
 //           }
-//         ]
-//       }
+//         ],
+//         model: "accounts/fireworks/models/deepseek-r1-0528",
+//         stream: false
+//       })
 
-const LLM_URL = 'https://81fc-128-239-2-78.ngrok-free.app/infer'
+
+const LLM_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent'
 
 // Load token on startup
-// fetch(chrome.runtime.getURL("config.json"))
-//   .then(response => response.json())
-//   .then(config => {
-//     hfToken = config.HF_TOKEN;
-//     console.log("✅ Token loaded");
-//   })
-//   .catch(err => console.error("❌ Failed to load config.json:", err));
+fetch(chrome.runtime.getURL("config.json"))
+  .then(response => response.json())
+  .then(config => {
+    apiKey = config.API_KEY
+    hfToken = config.HF_TOKEN;
+    console.log("✅ Token loaded");
+  })
+  .catch(err => console.error("❌ Failed to load config.json:", err));
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "askDeepSeek") {
     console.log("payload: ")
     console.log(message.payload);
-    const HF_TOKEN = hfToken; // Replace this with your actual token
+    const TOKEN = apiKey; // Replace this with your actual token
 
     fetch(LLM_URL, {
-      method: "POST", // ✅ Required for POST
+      method: "POST",
       headers: {
-        // "Authorization": `Bearer ${HF_TOKEN}`,
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json',
+        'X-goog-api-key': TOKEN,
       },
       body: JSON.stringify({
-        "text" : message.payload || "What is the capital of France?"
+        contents: [
+          {
+            parts: [
+              {
+                text: message.payload || "What is the capital of France? answer to the point"
+              }
+            ]
+          }
+        ]
       })
     })
       .then(response => response.json())
       .then(data => {
         console.log(data);
-        // sendResponse({ success: true, result: data.choices[0].message.content });
-        sendResponse({success: true, result: data})
+        sendResponse({ success: true, result: data["candidates"][0]["content"]["parts"][0]["text"] });
       })
       .catch(error => {
-        console.log(error.message);
         sendResponse({ success: false, error: error.message });
       });
 
